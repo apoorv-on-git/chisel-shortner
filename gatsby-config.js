973f134eb2a3370f835631e313c87c8a3d7bf4ad
apoorv-on-git <apoorvelous@gmail.com>
2022-06-16 08:53:32 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `shortner`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
